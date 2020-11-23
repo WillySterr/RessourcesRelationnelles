d2390dbcefb5ac1b5dd43fc9015035da11cfff78
Charly <willysterr@gmail.com>
2020-11-23 17:17:35 +0100

    init easyadmin

---
+++ projet_fil_rouge/src/Controller/Admin/EasyAdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Adminn;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EasyAdminController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Projet Fil Rouge');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'icon class', EntityClass::class);
+    }
+}
