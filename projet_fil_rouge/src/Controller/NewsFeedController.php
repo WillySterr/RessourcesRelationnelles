ae6fe86b8a9d2b7a6da3c9e3c169a564f24bdbe5
Girard <paul.girard1@viacesi.fr>
2020-12-05 16:59:07 +0100

    [Edit] Add icons and elements in NavBar && [Add] News Feed Controller

---
+++ projet_fil_rouge/src/Controller/NewsFeedController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RessourcesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewsFeedController extends AbstractController
+{
+    /**
+     * @Route("/", name="news_feed")
+     */
+    public function index(RessourcesRepository $ressourcesRepository): Response
+    {
+
+        $newsFeed = $ressourcesRepository->findAll();
+        return $this->render('news_feed/index.html.twig', [
+            'news' => $newsFeed,
+        ]);
+    }
+}
