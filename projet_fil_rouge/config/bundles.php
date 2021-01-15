948ee1fe4280e3b1cbda69eaeeed14bff40dc56e
Whyzen <girard.paul39@gmail.com>
2021-01-15 13:59:29 +0100

    [Add] config bundles

--- projet_fil_rouge/config/bundles.php
+++ projet_fil_rouge/config/bundles.php
@@ -16,4 +16,7 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
