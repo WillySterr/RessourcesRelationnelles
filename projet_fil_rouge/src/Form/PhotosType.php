ef9df44375930720e90060b0ab20d2ba68e8aa85
Whyzen <girard.paul39@gmail.com>
2021-01-15 18:04:08 +0100

    [Edit] update front end form

--- projet_fil_rouge/src/Form/PhotosType.php
+++ projet_fil_rouge/src/Form/PhotosType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Photos;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,25 +16,20 @@ class PhotosType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            //->add('published')
-            ->add('slug')
             ->add('titre')
             ->add('description')
-            /*->add('createdAt')
-            ->add('updatedAt')*/
             ->add('image', FileType::class,[
                 'label' => 'Image',
                 'multiple' => true,
                 'mapped' => false,
                 'required' => false
             ])
-            //->add('user')
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 "choice_label" => "name",
                 'multiple'=> true,
                 'expanded' => true
-            ])
+                ])
         ;
     }
 
