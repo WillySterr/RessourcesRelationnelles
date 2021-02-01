6e4ceeab55dfc716a31f45b8dfa170ffd413e445
WillySterr <willysterr@gmail.com>
2021-02-01 11:33:23 +0100

    add icon easyadmin

--- projet_fil_rouge/src/Controller/Admin/EasyAdminController.php
+++ projet_fil_rouge/src/Controller/Admin/EasyAdminController.php
@@ -90,11 +90,11 @@ class EasyAdminController extends AbstractDashboardController
         }
 
         if ($this->isGranted('ROLE_SUPERADMIN')) {
-            yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', Users::class)
+            yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-users', Users::class)
             ->setController(UsersCrudController::class);
         }
         if ($this->isGranted('ROLE_ADMIN') || $this->isGranted('ROLE_SUPERADMIN')) {
-            yield MenuItem::linkToCrud('Avatars', 'fa fa-icon', Avatars::class)
+            yield MenuItem::linkToCrud('Avatars', 'fa fa-user-circle', Avatars::class)
             ->setController(AvatarsCrudController::class);
         }
 
