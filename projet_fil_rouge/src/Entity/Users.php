3b6b77ad1b6a9e07128e6f3b8b7b08db767ca957
WillySterr <willysterr@gmail.com>
2021-02-02 15:07:55 +0100

    fix reger user

--- projet_fil_rouge/src/Entity/Users.php
+++ projet_fil_rouge/src/Entity/Users.php
@@ -68,7 +68,7 @@ class Users implements UserInterface
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\Regex(
-     *      pattern="/^((\+)33|0|0033)[1-9](\d{2}){4}$/g",
+     *      pattern="/^((\+)33|0|0033)[1-9](\d{2}){4}$/",
      *      message="Veuillez renseigner un numéto de téléphone valide."
      * )
      * @Assert\NotNull
@@ -79,7 +79,7 @@ class Users implements UserInterface
     /**
      * @ORM\Column(type="text")
      * @Assert\Regex(
-     *      pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$",
+     *      pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/",
      *      message="Votre mot de passe doit contenir au moins 10 caractères, une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial. "
      * )
      * @Assert\NotNull
@@ -164,8 +164,6 @@ class Users implements UserInterface
     /**
      * @ORM\ManyToOne(targetEntity=Avatars::class, inversedBy="users")
      * @Groups("fil_actu")
-     * @Assert\NotNull
-	   * @Assert\NotBlank
      * @Groups("ressource_comments")
      */
     private $avatar;
